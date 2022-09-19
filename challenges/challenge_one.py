d558676ea353c29018036f0c89455af3681b07f0
DevHarleyA <DevHarleyA@users.noreply.github.com>
2022-09-19 22:22:59 +0000

    commiting first challenge

---
+++ challenges/challenge_one.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def main():
+
+    # input asking for username 
+
+    name = input("What is your name there, stranger?")
+
+    # input asking for day
+
+    day = input("Okay, " + name + ".  What day is it today?")
+
+    # print statement
+
+    print("Hello, " + name + "! Happy " + day + "!")
+
+
+main()
